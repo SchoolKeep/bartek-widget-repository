d845cb2d3b53b4c0b80be44d9bb10487acd85c2c
Bartosz Glowacki <bartosz.glowacki@gainsight.com>
2026-03-27 14:12:09 +0100

    Sync with upstream: add connectors support, update react_widget

--- widgets/react_widget/vite.config.ts
+++ widgets/react_widget/vite.config.ts
@@ -3,19 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
   build: {
     lib: {
       entry: "src/main.tsx",
       formats: ["es"],
       fileName: "widget",
-    },
-    rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "react/jsx-runtime",
-        "react-dom/client",
-      ],
     },
   },
 });
